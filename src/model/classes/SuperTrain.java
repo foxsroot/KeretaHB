6a5c6141ea9b7fbe7e640465660e13059581b20b
JoChAl51 <jcanassgamers04@gmail.com>
2024-06-28 01:32:31 +0700

    Added The Train Class with The Extends.

--- src/model/classes/SuperTrain.java
+++ src/model/classes/SuperTrain.java
@@ -1,4 +1,28 @@
 package model.classes;
 
-public class SuperTrain {
+public class SuperTrain extends Train {
+    private String wifiName;
+    private String wifiPassword;
+
+    public SuperTrain(Carriage[] carriages, Integer speed, Victual victual, String wifiName, String wifiPassword) {
+        super(carriages, speed, victual);
+        this.wifiName = wifiName;
+        this.wifiPassword = wifiPassword;
+    }
+
+    public String getWifiName() {
+        return wifiName;
+    }
+
+    public void setWifiName(String wifiName) {
+        this.wifiName = wifiName;
+    }
+
+    public String getWifiPassword() {
+        return wifiPassword;
+    }
+
+    public void setWifiPassword(String wifiPassword) {
+        this.wifiPassword = wifiPassword;
+    }
 }
