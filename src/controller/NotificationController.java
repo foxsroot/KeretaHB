7523acb956adcc48158248945a396a47bffdfc28
Jer <rickoaxeldinata@gmail.com>
2024-07-05 17:41:55 +0700

    Added UI for send notification feature, fixed some bugs in send notification function

--- src/controller/NotificationController.java
+++ src/controller/NotificationController.java
@@ -10,16 +10,31 @@ import java.util.ArrayList;
 public class NotificationController {
     ConnectionHandler conn = new ConnectionHandler();
 
-    public boolean sendNotification(int user_id, String message) {
+    public boolean validateNotificationForm(String email, String title, String message) {
+        if (email == null || title == null || message == null) {
+            return false;
+        }
+
+        return (!email.isEmpty() && !title.isEmpty() && !message.isEmpty());
+    }
+
+    public boolean sendNotification(String email, String title, String message) {
+        int user_id = getIdByEmail(email);
+
+        if (user_id == -1) {
+            return false;
+        }
+
         conn.connect();
 
-        String query = "INSERT INTO notification(recipient_id, message, received_date) VALUES (?, ?, ?)";
+        String query = "INSERT INTO notification(recipient_id, title, message, received_date) VALUES (?, ?, ?, ?)";
 
         try {
             PreparedStatement stmt = conn.con.prepareStatement(query);
             stmt.setInt(1, user_id);
-            stmt.setString(2, message);
-            stmt.setDate(3, new java.sql.Date(System.currentTimeMillis()));
+            stmt.setString(2, title);
+            stmt.setString(3, message);
+            stmt.setTimestamp(4, new java.sql.Timestamp(System.currentTimeMillis()));
 
             stmt.executeUpdate();
         } catch (SQLException e) {
@@ -36,7 +51,7 @@ public class NotificationController {
         conn.connect();
         ArrayList<Notification> notifications = new ArrayList<>();
 
-        String query = "SELECT * FROM notification WHERE recipient_id=?";
+        String query = "SELECT * FROM notification WHERE recipient_id=? ORDER BY received_date DESC";
 
         try {
             PreparedStatement stmt = conn.con.prepareStatement(query);
@@ -44,7 +59,8 @@ public class NotificationController {
             ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) {
-                Notification notification = new Notification(rs.getInt("notification_id"), rs.getInt("recipient_id"), rs.getString("message"), rs.getDate("received_date"));
+                Notification notification = new Notification(rs.getInt("notification_id"), rs.getInt("recipient_id"), rs.getString("title"), rs.getString("message"), rs.getTimestamp("received_date"));
+                notifications.add(notification);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -53,5 +69,29 @@ public class NotificationController {
         }
 
         return notifications;
+    }
+
+    private int getIdByEmail(String email) {
+        conn.connect();
+
+        String query = "SELECT user_id FROM passenger WHERE email=?";
+
+        try {
+            PreparedStatement stmt = conn.con.prepareStatement(query);
+            stmt.setString(1, email);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return rs.getInt("user_id");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        } finally {
+            conn.disconnect();
+        }
+
+        return -1;
     }
 }
