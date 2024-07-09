eed141b710ffc1008d5fa28f84d17be97f046887
JoChAl51 <jcanassgamers04@gmail.com>
2024-07-10 06:34:31 +0700

    Init Authentication Controller.

---
+++ src/controller/AuthenticationController.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class AuthenticationController {
+    public boolean login(String userProfile, String password) {
+        return true;
+    }
+    public boolean logout(int userId) {
+        return true;
+    }
+}
